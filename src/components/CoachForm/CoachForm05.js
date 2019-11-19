fc26f5cbb1135a522c23f2802915182855009e6b
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-11-19 17:35:15 -0500

    CoachForm05

---
+++ src/components/CoachForm/CoachForm05.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+
+
+
+const CoachForm05 = () => {
+
+
+
+    return (
+        <div>
+            
+            <h2> Your coach profile is live!</h2>
+            <br/>
+            <h4>You can see it in the coaches</h4>
+        </div>
+    )
+
+};
+
+
+
+export default CoachForm05;
