e52f189e3490b6ee3f66400856361eb72e839b72
Jordan <jordandoan@hotmail.com>
2019-11-19 16:04:05 -0800

    Styled search box for coach list

--- src/components/CoachList/CoachList.js
+++ src/components/CoachList/CoachList.js
@@ -4,12 +4,34 @@ import './CoachList.scss'
 
 const CoachList = () => {
     return(
+      <div>
+        <h1>Interview Q</h1>
+        <div className="search-box">
+          Search Information
+          <form className="search-form">
+            <input placeholder="Search by keyword..." />
+            <div>
+              <select>
+                <option>Hi</option>
+              </select>
+              <select>
+                <option>Hi</option>
+              </select>
+              <select>
+                <option>Hi</option>
+              </select>
+            </div>
+            <button className="search-button">Search</button>
+          </form>
+        </div>
         <div className="coach-list">
+        
         <CoachCard />
         <CoachCard />
         <CoachCard />
         <CoachCard />
         </div>
+      </div>
     )
 }
 
