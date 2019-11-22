32625f16cfb0cd8c74484dfbf848e52b87c4ada8
Jonathan Ho <johnnyho82@gmail.com>
2019-11-22 11:00:24 -0500

    Add PrivateRoute (commented out in App.js)

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
 import React, { useState } from 'react';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom';
 
-import InterviewLandingPage from "./components/Landing Page/index.js"
-import CoachForm00 from "./components/CoachForm/CoachForm00";
+import InterviewLandingPage from './components/Landing Page/index.js';
+import CoachForm00 from './components/CoachForm/CoachForm00';
 import NavBar from './components/NavBar';
 import DatePicker from './components/DatePicker';
 import CoachList from './components/CoachList';
+import PrivateRoute from './components/PrivateRoute';
 
 import './index.scss';
 import './global/index.scss';
 
 function App() {
-  const [loggedin, setLoggedin] = useState(false);
-  return (
-  
-    <div className="App">
-      <Route
+	const [loggedin, setLoggedin] = useState(false);
+	return (
+		<div className='App'>
+			<Route
 				path='/'
 				render={props => (
 					<NavBar {...props} loggedin={loggedin} setLoggedin={setLoggedin} />
 				)}
 			/>
-      <Switch>
-      <Route exact path='/' component={InterviewLandingPage}/>
-      <Route path="/hey" component={DatePicker} />
-      <Route path='/addcoach' component={CoachForm00}/>
-      <Route path='/coachlist' component={CoachList} />
-      </Switch>
-    </div>
-  );
+			<Switch>
+				<Route exact path='/' component={InterviewLandingPage} />
+				<Route path='/hey' component={DatePicker} />
+				<Route path='/addcoach' component={CoachForm00} />
+				{/* <PrivateRoute path='/addcoach' component={CoachForm00} /> */}
+				<Route path='/coachlist' component={CoachList} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
