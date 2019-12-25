a6a1b9468a02e9f81006a881b01843a94a282748
Ryan Ziegenfus <thewebcoderz@gmail.com>
2019-12-25 00:43:21 -0500

    got part of signaling/chat working

--- index.js
+++ index.js
@@ -1,50 +1,49 @@
-let express = require('express.io');
+let express = require('express');
+let app = express();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
 let cors = require('cors');
-let server = express();
-server.use(cors());
-server.http().io();
 let PORT = 4000
 console.log('server started on port ' + PORT);
 
-server.use(express.static(__dirname + '/public'));
-server.use(cors({
-    origins: '*'
-  }));
-server.options("/*", function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(express.static(__dirname + '/public'));
 
-server.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    next();
-});
+io.on('connection', function(socket) {
+    socket.on('ready', function(data) {
 
-//joined room
-server.io.route('ready', function(req) {
-    req.io.join(req.data.chat_room)
-    req.io.join(req.data.signaling_room)
-    server.io.room(req.data).broadcast('announce', {
-        message: 'New client in the ' + req.data + ' room'
+        socket.join(data.chat_room)
+        socket.join(data.signaling_room)
+        socket.broadcast.to(data.signaling_room).emit('announce', {message: `${data.my_name} in the hizzouse`})
+        // socket.room(data).broadcast('announce', {
+        //         message: 'New client in the ' + data + ' room'
+        //     })
+        // server.io.join(data.chat_room)
+        // server.io.join(data.signaling_room)
+        // server.io.room(data).broadcast('announce', {
+        //     message: 'New client in the ' + data + ' room'
+        // })
     })
+    
+    //chat
+    socket.on('send',function(data) {
+        socket.broadcast.to(data.room).emit('message', {message: data.message, author: data.author})
+        // server.io.room(req.data.room).broadcast('message', {
+        //     message: req.data.message,
+        //     author: req.data.author
+        // });
+    })
+    
+    // //signaling
+    // socket.on('signal', function(req) {
+    //     console.log('this is signal');
+    //     req.io.room(req.data.room).broadcast('signaling_message', {
+    //         type: req.data.type,
+    //         message: req.data.message
+    //     });
+    // })
+    // console.log('helloworld')
 })
+//joined room
 
-//chat
-server.io.route('send', function(req) {
-    server.io.room(req.data.room).broadcast('message', {
-        message: req.data.message,
-        author: req.data.author
-    });
-})
-
-//signaling
-server.io.route('signal', function(req) {
-    req.io.room(req.data.room).broadcast('signaling_message', {
-        type: req.data.type,
-        message: req.data.message
-    });
-})
 
 server.listen(PORT);
